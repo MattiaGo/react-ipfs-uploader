5eb245210d2abfa13d035aad715663667deabb08
MattiaGo <91184227+MattiaGo@users.noreply.github.com>
2023-01-10 20:00:49 +0100

    fix

--- src/components/MultipleFilesUpload.js
+++ src/components/MultipleFilesUpload.js
@@ -20,8 +20,6 @@ const ipfs = ipfsHttpClient({
   });
 //const ipfs = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
-const all = require('it-all')
-
 export const MultipleFilesUpload = ({ setUrl }) => {
     const [files, setFiles] = useState([])
     const [filesUrl, setFilesUrl] = useState('')
